297ca903e4f440e9578b96498bd7c29e8ceafcb9
helmi <137972846+teathricalhood122@users.noreply.github.com>
2026-05-15 21:13:32 +0700

    Jobsheet12 praktikum 1

---
+++ jobsheet_12_DoubleLinkedList/Mahasiswa11.java
@@ -0,0 +1,23 @@
+package jobsheet_12_DoubleLinkedList;
+
+public class Mahasiswa11 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa11(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    public void tampil() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("-------------------------");
+    }
+}
---
+++ jobsheet_12_DoubleLinkedList/Node11.java
@@ -0,0 +1,13 @@
+package jobsheet_12_DoubleLinkedList;
+
+public class Node11 {
+    Mahasiswa11 data;
+    Node11  prev;
+    Node11 next;
+
+     public Node11(Mahasiswa11 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+     }
+}
